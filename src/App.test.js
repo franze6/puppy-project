d4a189b2eab3576e866018fb3eca7c41a9b59475
kandrusyak <kandrusyak@tsconsulting.com>
2021-09-29 17:04:20 +0300

    FIX: Edit tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ import { App } from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/123/i);
+  const linkElement = screen.getByText(/Андрусяк/i);
   expect(linkElement).toBeTruthy();
 });
