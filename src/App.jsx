b8296aa3d04a9f57c4b9a7d99ce2bba1394563ba
kandrusyak <kandrusyak@tsconsulting.com>
2021-09-29 16:59:35 +0300

    ADD: Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-import style from './App.scss';
+import Table from './Components/Table/Table';
 
 export const App = () => {
-  return <div className={style.main}>123</div>;
+  const columns = [
+    {
+      name: 'last_name',
+      display: 'Фамилия',
+      width: 100,
+    },
+    {
+      name: 'first_name',
+      display: 'Имя',
+      width: 100,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      last_name: 'Андрусяк',
+      first_name: 'Кирилл',
+    },
+    {
+      id: 2,
+      last_name: 'Андрюшин',
+      first_name: 'Александр',
+    },
+    {
+      id: 3,
+      last_name: 'Красавцев',
+      first_name: 'Сергей',
+    },
+  ];
+
+  return <Table columns={columns} rows={rows} />;
 };
