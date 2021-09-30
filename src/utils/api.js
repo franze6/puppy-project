be385ef3fa0d6a6e34da011d1d7a404f8ea9ed4c
Tatiana Kiseleva <tkiseleva@t1-consulting.ru>
2021-09-30 12:58:50 +0300

    Add GET request (fetch)

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,17 @@
-export async function getPersons() {
-  return [
-    {
-      id: 1,
-      last_name: 'Иванов',
-      first_name: 'Сергей',
-      second_name: 'Александрович',
-      birth_date: '1993-09-29',
-      tax_id: 345234123987,
-      insurance_number: 62153358681,
-      gender: 'm',
-    },
-    {
-      id: 2,
-      last_name: 'Фазанова',
-      first_name: 'Алла',
-      second_name: 'Николаевна',
-      birth_date: '2001-01-23',
-      tax_id: 453890561094,
-      insurance_number: 82309560064,
-      gender: 'w',
-    },
-    {
-      id: 3,
-      last_name: 'Вертинский',
-      first_name: 'Кирилл',
-      second_name: 'Михайлович',
-      birth_date: '1976-05-03',
-      tax_id: 271095780163,
-      insurance_number: 12487326490,
-      gender: 'm',
-    },
-  ];
+export async function getPersons(page = 1, page_size = 10) {
+  // eslint-disable-next-line camelcase
+  const url = `http://pet.kandrusyak.ru:8000/api/persons/?page=${page}&page_size=${page_size}`;
+  const resp = await fetch(url);
+  const json = await resp
+    .json()
+    .then(res => {
+      const obj = {
+        page_count: res.page_params.page_count,
+        record_count: res.page_params.record_count,
+        results: res.results,
+      };
+      return obj;
+    })
+    .catch(() => []);
+  return json;
 }
