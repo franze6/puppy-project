d1c92dae92e4a9f223b223ff6da5e9b21167c0e5
kandrusyak <kandrusyak@tsconsulting.com>
2021-10-01 10:48:53 +0300

    ADD: Routes with PersonDetail

--- src/Pages/PersonsDetail.jsx
+++ src/Pages/PersonsDetail.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
+import PropTypes from 'prop-types';
+
 import PersonDetail from '../Components/PersonDetail/PersonDetail';
 import { getPerson } from '../utils/api';
 
-const PersonsDetail = () => {
+const PersonsDetail = props => {
   const [person, setPerson] = useState({});
 
   useEffect(async () => {
-    const data = await getPerson();
+    const data = await getPerson(props?.match?.params?.id || 1);
     setPerson(data);
   }, []);
 
@@ -16,6 +18,10 @@ const PersonsDetail = () => {
       <PersonDetail person={person} />
     </div>
   );
+};
+
+PersonsDetail.propTypes = {
+  match: PropTypes.object,
 };
 
 export default PersonsDetail;
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,5 +1,9 @@
+import { Redirect } from 'react-router-dom';
+import React from 'react';
+
 import { App } from '../App';
 import Person from '../Pages/Person';
+import PersonsDetail from '../Pages/PersonsDetail';
 
 export const routes = [
   {
@@ -7,8 +11,19 @@ export const routes = [
     routes: [
       {
         path: '/',
+        redirect: '/123',
+        exact: true,
+        component: () => <Redirect to="/persons" />,
+      },
+      {
+        path: '/persons',
         exact: true,
         component: Person,
+      },
+      {
+        path: '/persons/:id',
+        exact: true,
+        component: PersonsDetail,
       },
     ],
   },
