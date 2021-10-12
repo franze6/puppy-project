48a2e743081a5776c4d2e1b626f96e7c3cdabece
Luydmila Rozhko <lrozhko@t1-consulting.ru>
2021-10-12 15:49:55 +0300

    FIX: component Map

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,42 +1,6 @@
 import React from 'react';
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
-// ymaps.ready(init);
-
-// const placemarks = [
-//   {
-//     latitude: 54.615371,
-//     longitude: 39.75275,
-//     hintContent: '<div class="map__hint">ул. Маяковского, д. 1 А</div>',
-//   },
-//   {
-//     latitude: 54.670144,
-//     longitude: 39.65143,
-//     hintContent: '<div class="map__hint">ул. Бирюзова, д. 10</div>',
-//   },
-//   {
-//     latitude: 54.629414,
-//     longitude: 39.71901,
-//     hintContent: '<div class="map__hint">ул. Первомайский пр-т, д. 56</div>',
-//   },
-//   {
-//     latitude: 54.63611,
-//     longitude: 39.650226,
-//     hintContent: '<div class="map__hint">ул. Новаторов, д. 2</div>',
-//   },
-//   {
-//     latitude: 54.622878,
-//     longitude: 39.752508,
-//     hintContent: '<div class="map__hint">ул. Радищева, д. 12</div>',
-//   },
-//   {
-//     latitude: 54.635214,
-//     longitude: 39.717447,
-//     hintContent: '<div class="map__hint">ул. 2-Бутырки, д. 11</div>',
-//   },
-// ];
-// const geoObjects = [];
-
 const mapData = {
   center: [54.628987, 39.739606],
   zoom: 11,
