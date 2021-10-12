ffea1946449a9b1202058a409958dcf25a9cb5bf
Tatiana Kiseleva <tkiseleva@t1-consulting.ru>
2021-10-12 17:01:42 +0300

    EDIT: searchPanel

--- src/Components/SearchPanel/SearchPanel.jsx
+++ src/Components/SearchPanel/SearchPanel.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import Search from '../Search/Search';
+
+import styles from './SearchPanel.scss';
+
 // eslint-disable-next-line react/prop-types
 const SearchPanel = () => {
-  return <div></div>;
+  return (
+    <div className={styles.wrapper}>
+      <Search />
+    </div>
+  );
 };
 
 export default SearchPanel;
