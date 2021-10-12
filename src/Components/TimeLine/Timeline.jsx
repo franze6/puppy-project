349235f8f86c57a20d6ca05856ab533c181b2d3c
Tatiana Kiseleva <tkiseleva@t1-consulting.ru>
2021-10-12 15:02:28 +0300

    EDIT: style Timeline

--- src/Components/TimeLine/Timeline.jsx
+++ src/Components/TimeLine/Timeline.jsx
@@ -12,19 +12,31 @@ const Timeline = () => {
     setList(data?.results || []);
   }, []);
   return (
-    <div className={style.timeline}>
-      <div className={style.line}> </div>
-      {list
-        .sort((a, b) => new Date(a.date) - new Date(b.date))
-        .map(curr => (
-          // eslint-disable-next-line react/jsx-key
-          <div className={style.list}>
-            <div className={style.curr}>{curr.project}</div>
-            <div className={style.curr}>{curr.position}</div>
-            <div className={style.circle}> </div>
-            <div className={style.curr}>{new Date(curr.date).getFullYear()}</div>
-          </div>
-        ))}
+    <div className={style.wrapper}>
+      <div className={style.timeline}>
+        <div className={style.line}> </div>
+        {list
+          .sort((a, b) => new Date(a.date) - new Date(b.date))
+          .map(curr => (
+            // eslint-disable-next-line react/jsx-key
+            <div className={style.list}>
+              <div className={style.circle}></div>
+              <div className={style.date}>{new Date(curr.date).getFullYear()}</div>
+              <div className={style.carrers}>
+                <div className={style.title}>Компания</div>
+                {curr.company.company_name}
+              </div>
+              <div className={style.carrers}>
+                <div className={style.title}>Проект</div>
+                {curr.project}
+              </div>
+              <div className={style.carrers}>
+                <div className={style.title}>Должность</div>
+                {curr.position}
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
