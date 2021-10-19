df15a723c35b9478df4f7644ffe3fe3ebeee86bd
Yulia Rudakova <yrudakova@t1-consulting.ru>
2021-10-19 11:14:10 +0300

    EDIT: PersonDetail

--- src/Components/PersonDetail/PersonDetail.jsx
+++ src/Components/PersonDetail/PersonDetail.jsx
@@ -12,6 +12,10 @@ import logo from './img/spanch-bob.jpg';
 const PersonDetail = ({ person }) => {
   const [list, setList] = useState([]);
   const [isBirthDay, setBirthDay] = useState(false);
+  const [editLink, setEditLink] = useState(false);
+  const [editLinkTeleg, setEditLinkTeleg] = useState(false);
+  const [editLinkMail, setEditLinkMail] = useState(false);
+  const [editLinkWhats, setEditLinkWhats] = useState(false);
 
   useEffect(async () => {
     const data = await getContactDetails();
@@ -26,101 +30,102 @@ const PersonDetail = ({ person }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.photo}>
-          <img className={styles.logo} src={logo} />
-          {isBirthDay && (
-            <div>
-              <Icon name="cake" className={styles.cake} />
-            </div>
-          )}
-        </div>
-        <div className={styles.fio}>{`${person.last_name} ${person.first_name} ${person.second_name}`}</div>
-        <div className={styles.title_contact}>
-          <div className={styles.label_contact}>
-            <Icon name="phone" />
-          </div>
-          <span className={styles.text}>
-            {list.map(contact =>
-              contact.type.type_name === 'Телефон' && contact.active ? (
-                <a className={styles.link} href={`tel:${contact.UID}`}>
-                  {contact.UID}
-                </a>
-              ) : (
-                ''
-              ),
+      <div className={styles.wraper}>
+        <div className={styles.header}>
+          <div className={styles.photo}>
+            <img className={styles.logo} src={logo} />
+            {isBirthDay && (
+              <div>
+                <Icon name="cake" className={styles.cake} />
+              </div>
             )}
-          </span>
-        </div>
-        <div className={styles.title_contact}>
-          <div className={styles.label_contact}>
-            <Icon name="telegram" />
           </div>
-          <span className={styles.text}>
-            {list.map(contact =>
-              contact.type.type_name === 'Telegram' && contact.active ? (
-                <a className={styles.link} href={`tg:${contact.UID}`}>
-                  {contact.UID}
-                </a>
-              ) : (
-                ''
-              ),
-            )}
-          </span>
         </div>
-        <div className={styles.title_contact}>
-          <div className={styles.label_contact}>
-            <Icon name="mail" />
-          </div>
-          <span className={styles.text}>
-            {list.map(contact =>
-              contact.type.type_name === 'e-mail' && contact.active ? (
-                <a className={styles.link} href={`mailto:${contact.UID}`}>
-                  {contact.UID}
-                </a>
-              ) : (
-                ''
-              ),
-            )}
-          </span>
-        </div>
-        <div className={styles.title_contact}>
-          <div className={styles.label_contact}>
-            <Icon name="whatsApp" />
-          </div>
-          <span className={styles.text}>
-            {list.map(contact =>
-              contact.type.type_name === 'WhatsApp' && contact.active ? (
-                <a className={styles.link} href={`whatsapp:${contact.UID}`}>
-                  {contact.UID}
-                </a>
-              ) : (
-                ''
-              ),
-            )}
-          </span>
+        <div>
+          <div className={styles.fio}>{`${person.last_name} ${person.first_name} ${person.second_name}`}</div>
+          <div className={styles.main}></div>
         </div>
       </div>
-      <div className={styles.main}>
-        <div className={styles.title}>
-          <div className={styles.label}> Дата рождения </div>
-          <span className={styles.text}>{`${new Date(person.birth_date).toLocaleDateString()}`}</span>
+      <div className={styles.header__link}>
+        <div className={styles.title_contact}>
+          <div className={styles.label_contact} onClick={() => setEditLink(!editLink)}>
+            <Icon name="phone" />
+          </div>
+          {editLink ? (
+            <span className={styles.text}>
+              {list.map(contact =>
+                contact.type.type_name === 'Телефон' && contact.active ? (
+                  <a className={styles.link} href={`tel:${contact.UID}`}>
+                    {contact.UID}
+                  </a>
+                ) : (
+                  ''
+                ),
+              )}
+            </span>
+          ) : (
+            ''
+          )}
         </div>
-        <div className={styles.title}>
-          <div className={styles.label}> Номер ИНН </div>
-          <span className={styles.text}>{`${person.tax_id}`}</span>
+        <div className={styles.title_contact}>
+          <div className={styles.label_contact} onClick={() => setEditLinkTeleg(!editLinkTeleg)}>
+            <Icon name="telegram" />
+          </div>
+          {editLinkTeleg ? (
+            <span className={styles.text}>
+              {list.map(contact =>
+                contact.type.type_name === 'Telegram' && contact.active ? (
+                  <a className={styles.link} href={`tg:${contact.UID}`}>
+                    {contact.UID}
+                  </a>
+                ) : (
+                  ''
+                ),
+              )}
+            </span>
+          ) : (
+            ''
+          )}
         </div>
-        <div className={styles.title}>
-          <div className={styles.label}> СНИЛС </div>
-          <span className={styles.text}>{`${person.insurance_number}`}</span>
+        <div className={styles.title_contact}>
+          <div className={styles.label_contact} onClick={() => setEditLinkMail(!editLinkMail)}>
+            <Icon name="mail" />
+          </div>
+          {editLinkMail ? (
+            <span className={styles.text}>
+              {list.map(contact =>
+                contact.type.type_name === 'e-mail' && contact.active ? (
+                  <a className={styles.link} href={`mailto:${contact.UID}`}>
+                    {contact.UID}
+                  </a>
+                ) : (
+                  ''
+                ),
+              )}
+            </span>
+          ) : (
+            ''
+          )}
         </div>
-        <div className={styles.title}>
-          <div className={styles.label}> Пол </div>
-          <span className={styles.text}>{`${person.gender}`}</span>
-        </div>
-        <div className={styles.title}>
-          <div className={styles.label}> Описание </div>
-          <span className={styles.text}>{`${person.description}`}</span>
+        <div className={styles.title_contact}>
+          <div className={styles.label_contact} onClick={() => setEditLinkWhats(!editLinkWhats)}>
+            <Icon name="whatsApp" />
+          </div>
+          {editLinkWhats ? (
+            <span className={styles.text}>
+              {list.map(contact =>
+                contact.type.type_name === 'WhatsApp' && contact.active ? (
+                  <a className={styles.link} href={`whatsapp:${contact.UID}`}>
+                    {contact.UID}
+                  </a>
+                ) : (
+                  ''
+                ),
+              )}
+            </span>
+          ) : (
+            ''
+          )}
         </div>
       </div>
     </div>
