ef3d55c37ae068a18795da0f282a2538a9d1e14c
Yulia Rudakova <yrudakova@t1-consulting.ru>
2021-10-19 11:42:28 +0300

    ADD: career position

--- src/Components/PersonDetail/PersonDetail.jsx
+++ src/Components/PersonDetail/PersonDetail.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 
-import { getContactDetails } from '../../utils/api';
+import { getContactDetails, getCareer } from '../../utils/api';
 
 import Icon from '../Kit/Icon/Icon';
 
@@ -11,6 +11,7 @@ import logo from './img/spanch-bob.jpg';
 
 const PersonDetail = ({ person }) => {
   const [list, setList] = useState([]);
+  const [career, setCareer] = useState({});
   const [isBirthDay, setBirthDay] = useState(false);
   const [editLink, setEditLink] = useState(false);
   const [editLinkTeleg, setEditLinkTeleg] = useState(false);
@@ -18,8 +19,10 @@ const PersonDetail = ({ person }) => {
   const [editLinkWhats, setEditLinkWhats] = useState(false);
 
   useEffect(async () => {
+    const dataCareer = await getCareer();
     const data = await getContactDetails();
     setList(data?.results || []);
+    setCareer(dataCareer?.results.sort((a, b) => new Date(b.date) - new Date(a.date))[0] || {});
   }, []);
 
   useEffect(() => {
@@ -43,7 +46,7 @@ const PersonDetail = ({ person }) => {
         </div>
         <div>
           <div className={styles.fio}>{`${person.last_name} ${person.first_name} ${person.second_name}`}</div>
-          <div className={styles.main}></div>
+          <div className={styles.main}>{career.position}</div>
         </div>
       </div>
       <div className={styles.header__link}>
